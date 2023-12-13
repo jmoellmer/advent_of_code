fb79cc41975d0dd11e49b5b1abda6906cd07e6b6
Jeff Moellmer <jeff.moellmer@gmail.com>
2023-12-13 09:38:52 -0600

    Update day2.py

--- 2023/day2/day2.py
+++ 2023/day2/day2.py
@@ -6,63 +6,63 @@ c = {
     'b': 14
 }
 
-gr = []
-gg = []
-gb = []
-
-gok = []
 
 def load(filename):
     with open(filename) as f:
         return f.readlines()
-    
+  
 
-def cube_color_idxs(line):
-    reds = [m.start(0) for m in re.finditer('red', line)]
-    greens = [m.start(0) for m in re.finditer('green', line)]
-    blues = [m.start(0) for m in re.finditer('blue', line)]
-    return reds, greens, blues
+def split_line(line):
+    game_n, game = line.split(':')    
+    game_n = int(re.findall('\d+', game_n)[0])
+    return game_n, game
 
 
-def cube_count(idx, line):    
-    i = idx-2
-    ch = line[i]
-    while ch != ' ':
-        i = i - 1
-        ch = line[i]
-    return int(line[i+1:idx-1])
-    
+def decode_game(game):
+    colors = [game[m.start(0)] for m in re.finditer('red|green|blue', game)]
+    n = [int(n) for n in re.findall(r'\d+', game)]
+    if len(colors) != len(n): raise ValueError("colors and n different lengths")
+    return n, colors
 
-def pass_colors(line, reds, greens, blues):
-    
-    for idx in reds:
-        n = cube_count(idx, line)
-        if n > c['r']:
-            return False
 
-    for idx in greens:
-        n = cube_count(idx, line)
-        if n > c['g']:
-            return False
+def max_cubes(colors, n, c):
+    max_c = -1
 
-    for idx in blues:
-        n = cube_count(idx, line)
-        if n > c['b']:
-            return False
-        
-    return True
+    for (idx, _) in enumerate(colors):
+        if colors[idx] == c:
+            max_c = max(max_c, n[idx])
 
+    return max_c
 
 def main():
-    lines = load('2023/day2/data.txt')
+    lines = load('advent_of_code/2023/day2/data.txt')
 
+    # part 1
     valid_games = []
-    for (i, game) in enumerate(lines):
-        r, g, b = cube_color_idxs(game)
-        if (pass_colors(game, r, g, b)):
-            valid_games.append(i + 1)
-    print(f"Valid Games: {sum(valid_games)}")
+    for line in lines:
+        game_n, game = split_line(line)
+        n, colors = decode_game(game)
+        max_n = max(n)
+        max_idx = n.index(max_n)
+        
+        if max_n < c[colors[max_idx]]:
+            valid_games.append(game_n)
 
+    print(f"Part 1: {sum(valid_games)}")
+
+    # part 2
+    power_sets = []
+    for line in lines:
+        game_n, game = split_line(line)
+        n, colors = decode_game(game)
+        
+        max_r = max_cubes(colors, n, 'r')
+        max_g = max_cubes(colors, n, 'g')
+        max_b = max_cubes(colors, n, 'b')
+
+        power_sets.append(max_r * max_g * max_b)
+
+    print(f"Part 2: {sum(power_sets)}")
 
 if __name__ == '__main__':
     main()
